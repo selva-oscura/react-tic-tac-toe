de7389afa17933b13f953e536764c43196e35f7e
Carol St.Louis <stlouis_c@yahoo.com>
2016-10-27 15:52:50 -0700

    imported and displayed Square Component with passed Board data

--- src/Board.js
+++ src/Board.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import Square from './Square';
 
 const Board = (props) => 
 		(
 			<div className="board">
 				{props.board.map((square,i) =>(
-							<p key={i}> square {square} </p>
+						<Square key={i} square={square} />
 					)
 				)}
 			</div>
