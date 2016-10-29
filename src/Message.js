2d4d7ce255262e44affa341b1fb140eb5b3aea81
Carol St.Louis <stlouis_c@yahoo.com>
2016-10-28 20:15:57 -0700

    added all messageShorthand types

--- src/Message.js
+++ src/Message.js
@@ -4,11 +4,55 @@ const Message = ({messageShorthand}) => {
 	console.log('messageShorthand', messageShorthand)
 	var messageContent;
 	switch(messageShorthand){
-		case 0:
-			messageContent = "X or O choice needs to go here";
+		case "XorO":
+			messageContent = (
+				<h3>
+					Play
+					<button>X</button>
+					or
+					<button>O</button>
+					?
+				</h3>
+			);
+			break;
+		case "compTurn":
+			messageContent = (
+				<h3>Computer's Turn</h3>
+			);
+			break;
+		case "userTurn":
+			messageContent = (
+				<h3>Your Turn</h3>
+			);
+			break;
+		case "XorO":
+			messageContent = (
+				<h3>
+					Congratulations! You won!
+					<button>Play Again?</button>
+				</h3>
+			);
+			break;
+		case "XorO":
+			messageContent = (
+				<h3>
+					Tie Game. 
+					<button>Play Again?</button>
+				</h3>
+			);
+			break;
+		case "XorO":
+			messageContent = (
+				<h3>
+					The Computer won. 
+					<button>Play Again?</button>
+				</h3>
+			);
 			break;
 		default:
-			messageContent = "Need to work up full list";
+			messageContent = (
+				<h3>Default</h3>
+			);
 			break;
 	}
 	return (
