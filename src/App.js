6fd476bae3ee61807d1f20e774c97690ad633910
Carol St.Louis <stlouis_c@yahoo.com>
2016-11-02 23:30:26 -0700

    replaced 'dumb' computer strategy with actual computer strategy; removed console.logs; changed staging of error messages and cleared error messages after game restart;

--- src/App.js
+++ src/App.js
@@ -17,12 +17,25 @@ const App = React.createClass ({
       errorMessage: undefined,
     }
   },
+  winningSolutions(){
+    return [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6]
+    ];
+  },
   playAgain(){
     let state = this.state;
     state.userIcon = undefined;
     state.playerTurn = undefined;
     state.board = [0,0,0,0,0,0,0,0,0];
     state.messageShorthand = "XorO";
+    state.errorMessage = undefined;
     this.setState(state);
   },
   concludeGame(winner){
@@ -39,16 +52,7 @@ const App = React.createClass ({
     this.setState(state);
   },
   checkGameWon(){
-    const winningSolutions = [
-      [0,1,2],
-      [3,4,5],
-      [6,7,8],
-      [0,3,6],
-      [1,4,7],
-      [2,5,8],
-      [0,4,8],
-      [2,4,6]
-    ];
+    const winningSolutions = this.winningSolutions();
     const board = this.state.board;
     // check for computer or user win
     let winner = false;
@@ -73,8 +77,36 @@ const App = React.createClass ({
     }
     return winner;
   },
+  checkPotentialWin(){
+    const winningSolutions = this.winningSolutions();
+    const board = this.state.board;
+    // check for player potential win
+    let targetSquares = {
+      squareToWin: null,
+      squareToBlock: null,
+    }
+    for (let i = 0; i<winningSolutions.length; i++){
+      let sum = 0; 
+      let emptySquare;
+      let possibleSolution = winningSolutions[i];
+      for(let j = 0; j<possibleSolution.length; j++){
+        sum += board[possibleSolution[j]];
+        if(board[possibleSolution[j]]===0){
+          emptySquare = possibleSolution[j];
+        }
+      }
+      if(sum === -2){
+        targetSquares.squareToWin = emptySquare;
+      }
+      if(sum === 2){
+        targetSquares.squareToBlock = emptySquare;
+      }
+    }
+    return targetSquares;
+  },
   updateUserIcon(icon){
     let state = this.state;
+    state.errorMessage = undefined;
     if(icon === "X"){
       state.userIcon = "X";
       state.playerTurn = "userTurn";
@@ -105,7 +137,9 @@ const App = React.createClass ({
       state.errorMessage = "That square already taken!";
     }else{
       state.board[i]=1;
-      this.setState(state);
+    }
+    this.setState(state);
+    if(!state.errorMessage){
       let winner = this.checkGameWon();
       if(winner){
         this.concludeGame(winner);
@@ -122,10 +156,74 @@ const App = React.createClass ({
   },
   computerPick(){
     let state = this.state;
-    let firstBlank = state.board.indexOf(0);
-    const timeout = Math.random()*1000+500;
+    let targetSquare;
+    let targetSquares = this.checkPotentialWin();
+    // check for square needed for immediate Win
+    if(targetSquares.squareToWin!==null){
+      targetSquare = targetSquares.squareToWin;
+    // check for square needed to preven immediate loss
+    }else if(targetSquares.squareToBlock!==null){
+      targetSquare = targetSquares.squareToBlock;
+    // pick center square if not already picked
+    }else if(state.board[4]===0){
+      targetSquare = 4;
+    // prevent opposite corners picked by opponent with adjacent corner as next pick leading to automatic win by opponent
+    }else if(state.board[0]===1 && state.board[8]===1){
+      if(state.board[1]!==-1 && state.board[2]!==-1 && state.board[5]!==-1){
+        if(state.board[1]===0){
+          targetSquare = 1;
+        }else if(state.board[5]===0){
+          targetSquare = 5;
+        }else if(state.board[2]===0){
+          targetSquare = 2;
+        }
+      }else if(state.board[7]!==-1 && state.board[6]!==-1 && state.board[3]!==-1){
+        if(state.board[7]===0){
+          targetSquare = 7;
+        }else if(state.board[3]===0){
+          targetSquare = 3;
+        }else if(state.board[6]===0){
+          targetSquare = 6;
+        }
+      }
+    // prevent opposite corners picked by opponent with adjacent corner as next pick leading to automatic win by opponent
+    }else if(state.board[2]===1 && state.board[6]===1){
+      if(state.board[1]!==-1 && state.board[0]!==-1 && state.board[3]!==-1){
+        if(state.board[1]===0){
+          targetSquare = 1;
+        }else if(state.board[3]===0){
+          targetSquare = 3;
+        }else if(state.board[0]===0){
+          targetSquare = 0;
+        }
+      }else if(state.board[5]!==-1 && state.board[8]!==-1 && state.board[7]!==-1){
+        if(state.board[5]===0){
+          targetSquare = 5;
+        }else if(state.board[7]===0){
+          targetSquare = 7;
+        }else if(state.board[8]===0){
+          targetSquare = 8;
+        }
+      }
+    }else{
+      // pick corner if available
+      [0, 2, 6, 8].forEach(function(spot){ 
+        if(state.board[spot] === 0 ){
+          targetSquare = spot;
+        }
+      });
+      if(targetSquare===undefined){
+        // else pick side
+        [1, 7, 3, 5].forEach(function(spot){ 
+          if(state.board[spot] === 0 ){
+            targetSquare = spot;
+          }
+        });
+      }
+    }
+    const timeout = Math.random()*500+250;
     setTimeout(() => {
-      state.board[firstBlank] = -1;
+      state.board[targetSquare] = -1;
       this.setState(state);
       let winner = this.checkGameWon();
       if(winner){
@@ -138,7 +236,6 @@ const App = React.createClass ({
     }, timeout);
   },
   render() {
-    console.log('this.state', this.state)
     return (
       <div className="App">
         <div className="App-header">
