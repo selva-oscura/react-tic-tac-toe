dba02261c75b33b19e5856312531fad5cea15182
Carol St.Louis <1.selva.oscura@gmail.com>
2017-05-20 19:23:33 -0700

    updated version; commented out console.logs

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var version = 1;
+var version = 2;
 var cacheName = 'tic-tac-toe-v' + version;
 var dataCacheName = 'tic-tac-toe-appData-v' + version;
 var filesToCache = [
@@ -18,22 +18,22 @@ var filesToCache = [
 ];
 
 self.addEventListener('install', function(e){
-	console.log('[ServiceWorker] install');
+	// console.log('[ServiceWorker] install');
 	e.waitUntil(
 		caches.open(cacheName).then(function(cache){
-			console.log('[ServiceWorker] Caching app shell');
+			// console.log('[ServiceWorker] Caching app shell');
 			return cache.addAll(filesToCache);
 		})
 	);
 });
 
 self.addEventListener('activate', function(e){
-	console.log('[ServiceWorker] Activated');
+	// console.log('[ServiceWorker] Activated');
 	e.waitUntil(
 		caches.keys().then(function(keyList){
 			return Promise.all(keyList.map(function(key){
 				if(key !== changeName && key !== dataCacheName){
-					console.log('[ServiceWorker] Removing old cache', key);
+					// console.log('[ServiceWorker] Removing old cache', key);
 					return caches.delete(key);
 				}
 			}));
