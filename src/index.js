741d0edfe16fbfdf505f3aab9c9636fb0d1b971d
Carol St.Louis <1.selva.oscura@gmail.com>
2017-06-23 20:13:00 -0700

    removed duplicate call to service-worker.js

--- src/index.js
+++ src/index.js
@@ -3,18 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-(function() {
-	if('serviceWorker' in navigator){
-		navigator.serviceWorker
-			.register('./service-worker.js')
-			.then(function(res) {
-				// console.log('Service Worker Registered', res); 
-		}).catch(function(err){
-			// console.log('ServiceWorker registration failed', err);
-		});
-	}
-})();
-
 ReactDOM.render(
   <App />,
   document.getElementById('root')
